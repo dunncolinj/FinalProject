f6de6ccc99ccdfc2761d27937f9508cf082fbc97
Leslie Counterman <LCounterman1@gmail.com>
2021-12-09 20:34:02 -0500

    PetModelsLayerClassesBuilt

--- FinalProject.Models/PetListItem.cs
+++ FinalProject.Models/PetListItem.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace FinalProject.Models
 {
-    class PetListItem
+    public class PetListItem
     {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public enum Species { get; set; }
     }
 }
