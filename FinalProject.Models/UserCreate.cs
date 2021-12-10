24432d3d12502dbc4c6df3ce7aaabf2099bc3dd3
Colin Dunn <dunncolinj@gmail.com>
2021-12-09 20:18:25 -0500

    User models

--- FinalProject.Models/UserCreate.cs
+++ FinalProject.Models/UserCreate.cs
@@ -1,30 +1,27 @@
-﻿using System;
+﻿using FinalProject.Data;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FinalProject.Data
+namespace FinalProject.Models
 {
-    public enum UserType { owner, petpro }
-    public class User
+    class UserCreate
     {
-        [Key]
-        public int Id { get; set; }
         [Required]
-        [MaxLength(100, ErrorMessage = "Limit of 100 characters")]
         public string Name { get; set; }
         [Required]
         public UserType Type { get; set; }
         [Required]
-        [MaxLength(12, ErrorMessage= "Maximum length of 12 characters including separators")]
+        [MaxLength(12, ErrorMessage = "Maximum length of 12 characters including separators")]
         public string Phone { get; set; }
         [Required]
-        [MaxLength(100, ErrorMessage = "Limit of 100 characters")] 
+        [MaxLength(100, ErrorMessage = "Limit of 100 characters")]
         public string Address { get; set; }
         [Required]
-        [MaxLength(25, ErrorMessage = "Limit of 25 characters")] 
+        [MaxLength(25, ErrorMessage = "Limit of 25 characters")]
         public string City { get; set; }
         [Required]
         [MaxLength(2, ErrorMessage = "State must be two-letter code")]
