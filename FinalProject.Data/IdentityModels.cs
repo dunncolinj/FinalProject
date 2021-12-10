82c0526475c7546809be06fb4350c14a49cdf93d
Anna Heselbarth <annaheselbarth@gmail.com>
2021-12-09 20:08:52 -0500

    DBSets added

--- FinalProject.Data/IdentityModels.cs
+++ FinalProject.Data/IdentityModels.cs
@@ -34,6 +34,9 @@ namespace FinalProject.Data
         }
 
         // Insert DbSet statements here for each table being added
+        public DbSet<User> Users { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<LostPet> LostPets { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
